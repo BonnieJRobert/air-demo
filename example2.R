790ff958f5e700ee558ac4abd5953cea9c1cb2e6
Bonnie Ashcroft <bonnie.ashcroft@gov.bc.ca>
2026-04-22 12:51:18 -0700

    make good examples

--- example2.R
+++ example2.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(dplyr)
 
-x = c(4, 5, 6, 7,8,9,10,11)
+x = c(4, 5, 6, 7, 8, 9, 10, 11)
 fav_animal <- "cat"
 likes_veggie <- "no"
 
@@ -9,6 +9,7 @@ info <- as.data.frame(
   x = x,
   f = fav_animal,
   v = likes_veggie,
-  w = x,a = f
+  w = x,
+  a = f
 )
 info |> pull(f)
