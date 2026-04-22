4e0b18ff85131d47eb444d5ba1713443eee1537b
Bonnie Ashcroft <bonnie.ashcroft@gov.bc.ca>
2026-04-22 09:39:04 -0700

    create project and examples

--- example1.R
+++ example1.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 x = c(4, 5, 6, 7, 8, 9, 10)
 fav_animal <- "cat"
-likes_veggie <- "no"
+likes_veggie <- "maybe"
 
 info <- as.data.frame(x = x, f = fav_animal, v = likes_veggie)
 info |> pull(f)
