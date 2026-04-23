d3066bcdf10d6357541e1d961314a7f54ef9d89a
Bonnie Ashcroft <bonnie.ashcroft@gov.bc.ca>
2026-04-23 11:38:37 -0700

    Update messy-example.R
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- messy-example.R
+++ messy-example.R
@@ -1,9 +1,9 @@
 library(ggplot2)
 library(dplyr)
 
-x=c(1,2,3,4,5,6,7,8,9,10,11,12)
-fav_animal<-c(  rep( "cat" ,length(x)-4) ,  rep("dog" , 4 ) )
-likes_veggie  <-"no"
+x = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+fav_animal <- c(rep("cat", length(x) - 4), rep("dog", 4))
+likes_veggie <- "no"
 
 info <-    data.frame(
 x = x,
