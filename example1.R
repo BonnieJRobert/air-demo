c35807a4962b18ce49ca1b4302b2bff8f325b947
Bonnie Ashcroft <bonnie.ashcroft@gov.bc.ca>
2026-04-23 09:57:10 -0700

    setup files for demo

--- example1.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-library(dplyr)
-
-x = c(4, 5, 6, 7, 8, 9,10,11)
-fav_animal <- "cat"
-likes_veggie <- "no"
-
-info <- as.data.frame(x = x, f = fav_animal, v = likes_veggie)
-info |> pull(f)
